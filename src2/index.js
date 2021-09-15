8c620e5e5eeef916e89a7e281f1d5b615aea9a0f
Zek Brown <zek.brown@live.nmit.ac.nz>
2021-09-16 11:20:13 +1200

    added 16/09 work

---
+++ src2/index.js
@@ -0,0 +1,28 @@
+// getElementById()
+// querySelector()
+
+// getElementByClassName()
+// getElementByTagName("li")
+// querySelectorAll("li.item")
+
+//innerHTML
+//textContent
+
+// const itemOne = getElementById("one")
+// DOM queries
+
+// document.getElementById()
+
+// const itemOne = document.getElementById("one");
+// itemOne.className = "Zek";
+
+//const tags = document.getElementByClassName("item")
+//if(tags.length >=1) (
+    //const selectedTag = tags.item(2)
+//)
+
+// const selectedTag = document.querySelectorAll(li.item)
+//NodeList
+// for (let i = 0; i < selectedTag.length; i++) {
+// selectedTag[i].className = "itemlist"
+//}
